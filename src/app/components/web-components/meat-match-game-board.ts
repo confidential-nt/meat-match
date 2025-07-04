4d0ffed49d2ee7859f3e5cfd121d19deb1d303d7
confidential-nt <youyoy689@gmail.com>
2025-07-04 22:39:27 +0900

    feat: 고기 블록 이미지 추가 및 MeatMatchGameBoard 컴포넌트에서 동적으로 렌더링되도록 구현

--- src/app/components/web-components/meat-match-game-board.ts
+++ src/app/components/web-components/meat-match-game-board.ts
@@ -1,13 +1,88 @@
 export default class MeatMatchGameBoard extends HTMLElement {
   private $root: ShadowRoot;
+  private numRows: number = 8;
+  private numCols: number = 8;
   constructor() {
     super();
     this.$root = this.attachShadow({ mode: 'closed' });
   }
 
   connectedCallback() {
-    const html = (v: TemplateStringsArray) => v[0];
+    this.render();
+  }
 
-    this.$root.innerHTML = html` <div class="board">hi</div> `;
+  private render() {
+    const style = ` <style>
+        :host {
+          --num-rows: ${this.numRows};
+          --num-cols: ${this.numCols};
+        }
+
+         * {
+          margin: 0;
+          padding: 0;
+          box-sizing: border-box;
+
+          touch-action: none;
+          user-select: none;
+          -webkit-user-drag: none;
+          -webkit-user-select: none;
+          -webkit-touch-callout: none;
+        }
+
+        .board {
+          --padding: 5px;
+          --border: 8px;
+          --board-width: 600px;
+          --board-inner-width: calc(
+            var(--board-width) - var(--border) * 2 - var(--padding) * 2
+          );
+           --meat-size: calc(var(--board-inner-width) / var(--num-cols));
+
+          padding: var(--padding); 
+
+          background-color:var(--color-board-bg);
+          border: var(--border) solid var(--color-board-border);
+          border-radius: 12px;
+
+          display: grid;
+          grid-template-rows: repeat(var(--num-rows), var(--meat-size));
+          grid-template-columns: repeat(var(--num-cols), var(--meat-size));
+
+          .block {
+            cursor: pointer;
+
+            img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+            }
+          }
+
+        }
+      </style>`;
+
+    const template = `
+    ${style}
+    <div class="board">
+        ${Array.from({ length: this.numRows * this.numCols })
+          .map(() => {
+            const type = this.getRandomType();
+            return `
+            <div class="block">
+              <img src="/meat/${type}.png" alt="${type}" draggable="false" />
+            </div>
+          `;
+          })
+          .join('')}
+    </div>    
+    `;
+
+    this.$root.innerHTML = template;
+  }
+
+  private getRandomType(): string {
+    const types = ['steak', 'chicken', 'bacon', 'rib', 'patty', 'sausage'];
+    return types[Math.floor(Math.random() * types.length)];
   }
 }
