cce0d123a06df05f460c96acead65942b00c61b7
confidential-nt <youyoy689@gmail.com>
2025-07-05 21:04:03 +0900

    feat: 헤더 및 게임 설명 추가

---
+++ src/app/components/layout/Header/Header.tsx
@@ -0,0 +1,9 @@
+import styles from './Header.module.css';
+
+export const Header = () => {
+  return (
+    <header className={styles.header}>
+      <h1>LET&apos;S MEAT MATCH</h1>
+    </header>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,28 @@
 import MeatMatchWrapper from '@/app/components/MeatMatchGameBoardWrapper';
 import styles from './page.module.css';
+import { Header } from './components/layout/Header/Header';
 
 export default function Home() {
   return (
     <div className={styles.page}>
+      <Header />
       <MeatMatchWrapper />
+      <section className={styles.instructions}>
+        <h3>🔪 BUTCHER&apos;S INSTRUCTIONS 🔪</h3>
+        <div>
+          <p>• Click a meat piece to select it (it will glow with bloodlust)</p>
+          <p>• Click an adjacent piece to swap and create matches</p>
+          <p>• Match 3+ identical meats to send them to the slaughterhouse</p>
+          <p>
+            • Each massacre earns you points in this carnivorous capitalism!
+          </p>
+          <p>
+            {
+              '"Remember: No animal was harmed in the making of this game... yet. 😈"'
+            }
+          </p>
+        </div>
+      </section>
     </div>
   );
 }
